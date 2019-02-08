29642041d7e8424224599e4b33da57e5513fa977
Twilit <kinlongsha@hotmail.co.uk>
2019-02-08 15:05:06 +0000

    Feedback + Tweak

---
+++ Project Labrat/Assets/Scripts/PlayerMimimapIcon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMimimapIcon : MonoBehaviour 
+{
+    GameObject player;
+
+    void Start () 
+	{
+        player = GameObject.FindGameObjectWithTag("Player");
+	}	
+
+	void Update () 
+	{
+        transform.eulerAngles = new Vector3(180, 0, player.transform.eulerAngles.y);
+	}
+}
