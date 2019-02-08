d811e4e45a9003aa6b166909a016fa069856c19f
Twilit <kinlongsha@hotmail.co.uk>
2019-02-08 13:59:38 +0000

    UI: Minimap Adjusted

---
+++ Project Labrat/Assets/Scripts/Health.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    [SerializeField] int maximumHealth = 100;
+    int currentHealth = 100;
+
+
+    void Start()
+    {
+        // this means that crrentHealth will be = to the maxhealth that i have set.
+        currentHealth = maximumHealth;
+
+
+    }
+    public bool IsDead { get { return currentHealth <= 0; } }
+
+    public int getHealth()
+    {
+        return currentHealth;
+    }
+
+    public int getMaxHealth()
+    {
+        return maximumHealth;
+    }
+    public void Damage(int damageValue)
+    {
+        //Debug.Log("Damage");
+        currentHealth -= damageValue;
+        // this meas if the currenthealth = 0 then the player object will be destory form the game.
+
+
+    }
+
+
+}
+         
+
---
+++ Project Labrat/Assets/Scripts/SawDamage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawDamage : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider collision)
+    {
+
+        if (collision.tag == "Player")
+        {
+
+            var hit = collision.gameObject;
+            var health = hit.GetComponent<Health>();
+            if (health != null)
+            {
+
+                health.Damage(100);
+            }
+        }
+    }
+}
