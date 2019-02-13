d413543ea4e23219921189350d125ee432cc00ee
nathanielMeakin <44022056+nathanielMeakin@users.noreply.github.com>
2019-02-13 10:33:55 +0000

    nathaniel door oping  code

---
+++ Project Labrat/Assets/DoorTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour {
+
+    [SerializeField] GameObject Door;
+    bool isOpened = false;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (isOpened == false)
+        {
+            isOpened = true;
+            Door.transform.position += new Vector3(0, 4, 0);
+            StartCoroutine("doorStoop");
+        }
+
+
+    }
+
+    IEnumerator doorStoop()
+    {
+        yield return new WaitForSeconds(Random.Range(3,6));
+        if (isOpened == true)
+        {
+            isOpened = false;
+            Door.transform.position += new Vector3(0, -4, 0);
+        }
+
+    }
+}
