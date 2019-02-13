b5b8d24242ea6e11756b4c43125e1479fc67e72b
Twilit <kinlongsha@hotmail.co.uk>
2019-02-13 12:33:37 +0000

    Coin Grab Indicator

--- Project Labrat/Assets/Scripts/CoinScript.cs
+++ Project Labrat/Assets/Scripts/CoinScript.cs
@@ -6,18 +6,50 @@ using UnityEngine.UI;
 public class CoinScript : MonoBehaviour
 {
     Text coinCounter;
+    GameObject player;
+
     public static int coins = 0;
 
     void Start()
     {
+        transform.GetChild(0).gameObject.SetActive(false);
+
         coinCounter = GameObject.FindGameObjectWithTag("CoinCounter").GetComponent<Text>();
+        player = GameObject.FindGameObjectWithTag("Player");
+       
+    }
+
+    private void OnMouseEnter()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) < 10)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        transform.GetChild(0).rotation = Quaternion.LookRotation(transform.position - player.transform.position);
+    }
+
+    private void OnMouseExit()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) < 10)
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
     }
 
     private void OnMouseDown()
     {
-        coins += 1;
-        coinCounter.text = "x " + coins;
+        if (Vector3.Distance(transform.position, player.transform.position) < 10)
+        {
+            print(Vector3.Distance(transform.position, player.transform.position));
 
-        Destroy(gameObject);
+            coins += 1;
+            coinCounter.text = "x " + coins;
+
+            Destroy(gameObject);
+        }
     }
 }
