a3b562111d0ba90f7e1278ca180055af7249ffc8
JClarke97 <44021611+JClarke97@users.noreply.github.com>
2019-02-15 02:46:34 +0000

    start of  randmom audio and win screen

---
+++ Project Labrat/Assets/Scripts/SoundPlayingAtSetIntervals.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundPlayingAtSetIntervals : MonoBehaviour
+{
+    public float time = 30f; //30 seconds for you
+    AudioSource audiosource;
+
+    public AudioClip MusicSource;
+
+    public void Update()
+
+    {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+        }
+        else
+        {
+            Debug.Log("Play Audio Here -- Timer Over!!");
+            audiosource.PlayOneShot(MusicSource);
+        }
+
+
+    }
+}
